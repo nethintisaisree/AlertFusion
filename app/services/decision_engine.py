a358d0c9ccaeb8430e91ba5d6f0c7cecfe7b866a
nethintisaisree <nethintisaisree13@gmail.com>
2026-04-17 17:59:01 +0530

    update

--- app/services/decision_engine.py
+++ app/services/decision_engine.py
@@ -24,7 +24,8 @@ CRITICAL_KEYWORDS = {
     "explosion", "dying", "suicide", "stab", "knife",
     "drowning", "choking", "overdose", "poisoning", "kidnapping",
     "robbery", "robber", "burglar", "burglary", "intruder",
-    "thief inside", "still inside", "trapped inside","gas leaking"
+    "thief inside", "still inside", "trapped inside","gas leaking","bomb"
+    "hostage","assault","abuse","domestic violence"
 }
 
 SAFETY_OVERRIDE_KEYWORDS = {
